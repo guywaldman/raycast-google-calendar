4b1f1d13c675aedcfea395e4857a70d938dea7ef
Guy Waldman <6546430+guywaldman@users.noreply.github.com>
2024-06-20 21:27:30 +0300

    Add Google OAuth client ID as preference

--- src/lib/raycast/raycast-google-auth-service.ts
+++ src/lib/raycast/raycast-google-auth-service.ts
@@ -1,6 +1,9 @@
+import { getPreferenceValues } from "@raycast/api";
 import { OAuthService } from "@raycast/utils";
 
+const { googleClientId } = getPreferenceValues();
+
 export const RaycastGoogleOAuthService = OAuthService.google({
-  clientId: "786675105585-3d5p5en3vnj9ep7ujfkbbggso1tucckf.apps.googleusercontent.com",
+  clientId: googleClientId,
   scope: "https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/meetings.space.created",
 });
