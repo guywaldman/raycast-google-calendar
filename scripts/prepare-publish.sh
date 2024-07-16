420c901cbc74363c378f7344749fab954d6072d3
Guy Waldman <6546430+guywaldman@users.noreply.github.com>
2024-07-17 02:40:33 +0300

    Bootstrap extension (#2)
    * Add basic calendars view
    
    * Add views to view calendars, see upcoming events and create an event
    
    * Add arguments to event creation page
    
    * Update icon
    
    * Indicate events with a conference
    
    * Refactor
    
    * Add more details for events
    
    * Improve upcoming events display
    
    * Refactor
    
    * Improve meeting event UX
    
    * Add Google Meet API client
    
    Scrapping this since conference records don't seem to work. Might be a Google Workspace thing.
    
    * Scrap GMeet client & fix linting issues
    
    * Improve formatted time
    
    * Add Google OAuth client ID as preference
    
    * Update README.md
    
    * Update README
    
    * Add script to publish to Raycast store
    
    * Add screenshots to README

---
+++ scripts/prepare-publish.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# This script prepares the extension for publishing to the Raycast store.
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ROOT_DIR=$(realpath "$SCRIPT_DIR/..")
+
+RAYCAST_EXTENSIONS_DIR_NAME="raycast-extensions"
+EXTENSION_NAME="google-calendar"
+
+# check if does not already exist
+if [ -d $ROOT_DIR/$RAYCAST_EXTENSIONS_DIR_NAME ]; then
+	echo "'$ROOT_DIR/$RAYCAST_EXTENSIONS_DIR_NAME' directory exists. Skipping clone."
+else
+	git clone https://github.com/guywaldman/raycast-extensions --depth=1 --branch=master $RAYCAST_EXTENSIONS_DIR_NAME
+fi
+
+echo "Copying extension to $RAYCAST_EXTENSIONS_DIR_NAME/$EXTENSION_NAME"
+mkdir -p $RAYCAST_EXTENSIONS_DIR_NAME/$EXTENSION_NAME
+rsync -av . $RAYCAST_EXTENSIONS_DIR_NAME/extensions/$EXTENSION_NAME --exclude=.git --exclude=node_modules --exclude=$RAYCAST_EXTENSIONS_DIR_NAME --exclude=scripts --exclude=.vscode
+
+echo "Creating new commit"
+pushd $RAYCAST_EXTENSIONS_DIR_NAME
+git checkout main
+git add extensions/$EXTENSION_NAME
+git commit -m "Update $EXTENSION_NAME"
+git push
+popd
+
+echo "Done! Create a pull request for https://github.com/raycast/extensions"
